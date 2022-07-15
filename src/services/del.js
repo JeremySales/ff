8ae708a58d2005c1c3617d0ee7a7afe2d056f8c5
JeremySales <salesjeremy37@gmail.com>
2022-07-14 21:51:54 -0500

    fixed the delete

--- src/services/del.js
+++ src/services/del.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export async function deleteMovie (movieName) {
-    await axios.delete("http://localhost:8000/", {data: {
+export  function deleteMovie (movieName) {
+    axios.delete("http://localhost:8000/", {data: {
       name: movieName}
       });
 };
