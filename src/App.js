04ae7a5cbd5b6262392d87b0ff7afd6db626c179
JeremySales <salesjeremy37@gmail.com>
2022-08-01 18:54:40 -0500

    normalized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import './css/normalize.css'
 import './App.css';
 import Header from './components/header';
 import {Button, TextField} from '@mui/material';
